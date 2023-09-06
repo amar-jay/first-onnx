33aa8cfb46075a75acc67545783b66a4c7888ed7
amar-jay <64834413+amar-jay@users.noreply.github.com>
2023-09-06 02:15:36 +0000

    🤧

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import reactLogo from './assets/react.svg'
+import { useOnnxModel } from './hooks/use-onnx'
+import viteLogo from '/vite.svg'
 
 function App() {
   const [count, setCount] = useState(0)
+  const {
+    state
+  } = useOnnxModel()
 
   return (
     <>
@@ -21,9 +25,10 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <div>
+            <h3>Sadness</h3>
+            <p>{state}</p>
+        </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
---
+++ app/src/hooks/use-onnx.tsx
@@ -0,0 +1,70 @@
+import ort, { InferenceSession } from 'onnxruntime-web'
+import { useEffect, useRef, useState } from 'react'
+import { tokenizer } from './tokenizer'
+import { sigmoid, EMOTIONS } from '../utils'
+const MODEL_NAME = 'model.onnx'
+type State = 'downloading' | 'warming-up' |'ready' | 'error' | 'unknown'
+
+type Emoji = [emotion: string, probability: bigint | number]
+
+async function inference(session: InferenceSession): [duration: number, Emoji[]] {
+	const start = performance.now();
+	const encoded = await tokenizer.tokenize(text)
+
+	if (encoded.length === 0) {
+		throw new Error("Empty encoding. Error in tokenizing")
+		// return [0.0, null]
+	}
+
+	const output = await session.run(model_input(encoded), ['output'])
+	const duration = performance.now() - start
+	const probabilities = output['output'].data.map(sigmoid)
+
+	const resultList:Emoji[] = []
+	for (let i in probabilities) {
+		resultList.push([EMOTIONS[i], probabilities[i]])
+	}
+
+	return [duration,resultList];
+
+}
+
+const model_input = (encoded: number[]) => {
+	encoded = [101, ...encoded, 102]
+	const inputArray:bigint[] = encoded.map(i => BigInt(i))
+	const attentionMaskArray:bigint[] = new Array(encoded.length + 2).fill(BigInt(1))
+	const tokenTypeArray:bigint[] = new Array(encoded.length + 2).fill(BigInt(0))
+	
+	const sequenceLen = inputArray.length;
+	const inputIds = new ort.Tensor('int64', BigInt64Array.from(inputArray), [1, sequenceLen])
+	const attentionMask = new ort.Tensor('int64', BigInt64Array.from(attentionMaskArray), [1, sequenceLen])
+	const tokenTypeIds = new ort.Tensor('int64', BigInt64Array.from(tokenTypeArray), [1, sequenceLen])
+	
+	return {
+		inputIds, 
+		attentionMask,
+		tokenTypeIds
+	}
+}
+
+export function useOnnxModel(){
+	const [state, setState] = useState<State>('unknown')
+	const session = useRef<Promise<InferenceSession>|undefined>()
+
+	useEffect(() => {
+		setState('downloading')
+		session.current = ort.InferenceSession.create(MODEL_NAME, {
+			// webgl: true,
+			executionProviders: ['webgl'],
+			graphOptimizationLevel: 'all',
+		})
+		session.current.then((s) => {
+			setState('warming-up')
+			inference(s)
+		})
+	}, [])
+	return {
+		state
+	}
+
+}
